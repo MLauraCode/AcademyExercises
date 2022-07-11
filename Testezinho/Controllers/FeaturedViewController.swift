34c3451a85c58102586e3c9da63073b09737ff9e
MLauraCode <mlbo@academy.cin.ufpe.br>
2022-07-11 17:17:05 -0300

    Implementa titulos dos filmes

---
+++ Testezinho/Controllers/FeaturedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FeaturedViewController.swift
+//  Testezinho
+//
+//  Created by MLaura on 04/07/22.
+//
+
+import UIKit
+
+class FeaturedViewController: UIViewController, UICollectionViewDataSource {
+ 
+    @IBOutlet var upcomingCollectionView: UICollectionView!
+    @IBOutlet var nowPlayingCollectionView: UICollectionView!
+    @IBOutlet var popularCollectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        popularCollectionView.dataSource = self
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "popularCell", for: indexPath) as? PopularCollectionViewCell{
+        cell.titleLabel.text = "Título do Filme"
+        cell.image.image = UIImage()
+        return cell
+        }
+        return UICollectionViewCell()
+    }
+}
+
---
+++ Testezinho/View/PopularCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PopularCollectionViewCell.swift
+//  Testezinho
+//
+//  Created by MLaura on 11/07/22.
+//
+
+import UIKit
+class PopularCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
+}
